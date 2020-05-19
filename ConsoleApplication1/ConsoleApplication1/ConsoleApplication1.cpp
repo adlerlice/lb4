40b0a2a7310240f89b421f3539dadde23e4f8887
adlerlice <adlerlice@gmail.com>
2020-05-20 06:18:08 +0700

    first version

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int Rec(int i, int n, int step, int current) {
+	return i < n ? Rec(i + 1, n, step, current + step) : current;
+}
+
+int main()
+{
+	int step = 4;
+	int n = 4;
+	std::cout << Rec(1, n, step, 0) << "\n";
+	system("pause");
+	return(0);
+}
